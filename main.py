c697e388ebaf5e9735dfd31943bf8b753940a08e
xavier-delfini <91735539+xavier-delfini@users.noreply.github.com>
2023-02-02 11:28:07 +0100

    Ajout d'une première todolist

---
+++ main.py
@@ -0,0 +1,15 @@
+#TODO:Aide a la disposition des pieces (mettre dans un array)https://static.wikia.nocookie.net/tetrisconcept/images/3/3d/SRS-pieces.png/revision/latest?cb=20060626173148
+#TODO:Taille d'une grille de tetris
+#   Hauteur: 22
+#   Largueur: 10
+#TODO:Possibilité de tourner les pieces(indépendant du reset de la grille)
+#TODO:Fonction actualisation de la grille + descente de la piece (Optionnel: Augmenter cette vitesse avec le temps pour augmenter la difficulté)
+#   Faire aussi une fonction en cas de lignes (Les lignes au dessus de la ligne complété doivent être décaler de x lignes complété
+#TODO:Spawn location : https://tetris.fandom.com/wiki/Spawn_Location (Indiquer au minimum la piece qui va suivre)
+#TODO:Système de score
+#   Single: 1 point
+#   Double: 3 points
+#   Triple: 5 points
+#   Tetris: 8 points
+
+import pygame
