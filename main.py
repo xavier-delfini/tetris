ea12b51a61fea56ca8f9f133f694280f1b994dd4
xavier-delfini <91735539+xavier-delfini@users.noreply.github.com>
2023-02-03 08:37:52 +0100

    Finalisation de la fonction rotation et début de la fonction draw_pieces

--- main.py
+++ main.py
@@ -36,17 +36,21 @@ while pygame.event.wait().type != pygame.QUIT:
     pass
 """
 
+def draw_pieces():
+
 def rotation(piece):
     match len(piece):
 
         #Square
         case 2:
-            pass
+            new_piece=piece
         #L reverseL,S_Block,Z_block
         case 3:
             new_piece=[[0,0,0],
                        [0,0,0],
                        [0,0,0]]
+            #TODO:A optimiser
+
             new_piece[0][0]=piece[2][0]
             new_piece[0][1]=piece[1][0]
             new_piece[0][2]=piece[0][0]
@@ -56,31 +60,36 @@ def rotation(piece):
             new_piece[2][0]=piece[2][2]
             new_piece[2][1]=piece[1][2]
             new_piece[2][2]=piece[0][2]
-            print(new_piece)
-            return new_piece
 
         #Line
         case 4:
-            pass
-Line = [[[0, 0, 0, 0],
+            new_piece = [[0, 0, 0,0],
+                         [0, 0, 0,0],
+                         [0, 0, 0,0],
+                         [0, 0, 0,0]]
+            #TODO:A obtimiser aussi
+            new_piece[0][1]=piece[2][0]
+            new_piece[0][2]=piece[1][0]
+            new_piece[1][0]=piece[3][1]
+            new_piece[1][1]=piece[2][1]
+            new_piece[1][2]=piece[1][1]
+            new_piece[1][3]=piece[0][1]
+            new_piece[2][0]=piece[0][2]
+            new_piece[2][1]=piece[2][2]
+            new_piece[2][2]=piece[1][2]
+            new_piece[2][3]=piece[3][2]
+            new_piece[3][1]=piece[2][3]
+            new_piece[3][2]=piece[1][3]
+            print(new_piece[0])
+            print(new_piece[1])
+            print(new_piece[2])
+            print(new_piece[3])
+            print("")
+            return new_piece
+Line = [[0, 0, 0, 0],
          [1, 1, 1, 1],
          [0, 0, 0, 0],
-         [0, 0, 0, 0]],
-
-        [[0, 0, 1, 0],  #[1,0] passe a [0,2]
-         [0, 0, 1, 0],  #[1,1] passe a [1,2]
-         [0, 0, 1, 0],  #[1,2] passe a [2,2]
-         [0, 0, 1, 0]], #[1,3] passe a [3,2]
-
-        [[0, 0, 0, 0],  # [0,2] passe a [2,0]
-        [0, 0, 0, 0],   # [1,2] passe a [2,1]
-        [1, 1, 1, 1],   # [2,2] passe a [2,2]
-        [0, 0, 0, 0]],  # [3,2] passe a [2,3]
-
-        [[0, 1, 0, 0],  # [2,0] passe a [0,1]
-        [0, 1, 0, 0],   # [2,1] passe a [1,1]
-        [0, 1, 0, 0],   # [2,2] passe a [2,1]
-        [0, 1, 0, 0]],  # [2,3] passe a [3,1]
+         [0, 0, 0, 0]
         ]
 
 Reverse_L = [[1, 0, 0],
@@ -113,15 +122,15 @@ window = pygame.display.set_mode((270, 570))
 for i in range(10, 571, 25):
     pygame.draw.line(window, (255, 255, 255), (10, i), (260, i))
     pygame.draw.line(window, (255, 255, 255), (i, 10), (i, 560))
-    pygame.draw.rect(window, (0, 255, 255), ((12, 12), (22, 22)), 0)
-    pygame.draw.rect(window, (0, 255, 255), ((37, 12), (22, 22)), 0)
-    pygame.draw.rect(window, (0, 255, 255), ((62, 12), (22, 22)), 0)
-    pygame.draw.rect(window, (0, 255, 255), ((87, 12), (22, 22)), 0)
+pygame.draw.rect(window, (0, 255, 255), ((12, 12), (22, 22)), 0)#Bloc de taille 22:22 avec 25 pixels de décalage pour allignement avec ligne
+pygame.draw.rect(window, (0, 255, 255), ((37, 12), (22, 22)), 0)
+pygame.draw.rect(window, (0, 255, 255), ((62, 12), (22, 22)), 0)
+pygame.draw.rect(window, (0, 255, 255), ((87, 12), (22, 22)), 0)
 
 pygame.display.update()
-T_Block=rotation(T_Block)
-T_Block=rotation(T_Block)
-T_Block=rotation(T_Block)
-T_Block=rotation(T_Block)
+Line=rotation(Line)
+Line=rotation(Line)
+Line=rotation(Line)
+Line=rotation(Line)
 while pygame.event.wait().type != pygame.QUIT:
     pass
